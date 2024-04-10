c78dd433d320dc4c6f5f4b893692a70ac9c87a9b
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-04-10 18:11:20 +0200

    Update The Title

--- pages/workspaces/[id]/index.tsx
+++ pages/workspaces/[id]/index.tsx
@@ -107,7 +107,7 @@ export default function WorkspaceDetail() {
 					<main className="flex flex-1 bg-white flex-col gap-4 p-4 lg:gap-6 lg:p-6">
 						<div className="flex items-center">
 							<h1 className="text-lg font-semibold md:text-2xl">
-								Workspaces
+								Bases
 							</h1>
 						</div>
 						<div
